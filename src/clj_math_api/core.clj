7c322ba8e30138b3cd9d26dabd8f988f0346d90f
jbranchaud <jbranchaud@gmail.com>
2015-11-21 13:00:37 -0600

    Add the plus route.

--- src/clj_math_api/core.clj
+++ src/clj_math_api/core.clj
@@ -4,7 +4,8 @@
             [ring.adapter.jetty :as jetty]))
 
 (defroutes app
-  (GET "/" [] "Hello, World!"))
+  (GET "/" [] "Hello, World!")
+  (GET "/:num1/plus/:num2" [num1 num2] (str (+ (Integer. num1) (Integer. num2)))))
 
 (defn -main []
   (jetty/run-jetty app {:port 3000}))
